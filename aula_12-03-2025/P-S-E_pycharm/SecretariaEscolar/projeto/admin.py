4a65c8c11856e8514b68a4fa8c8b6fb73f77d8cd
JMBIGMAC <134791439+JMBIGMAC@users.noreply.github.com>
2025-04-02 13:55:02 +0000

    0.0.3

--- aula_12-03-2025/P-S-E_pycharm/SecretariaEscolar/projeto/admin.py
+++ aula_12-03-2025/P-S-E_pycharm/SecretariaEscolar/projeto/admin.py
@@ -1,21 +1,39 @@
 from django.contrib import admin
-from .models import Responsavel, Aluno, Professor
-class ResponsaveisAdimin(admin.ModelAdmin):
-    list_display = ('id','fist_name','last_name','phone_number','email','adress','cpf','birthday')
-    list_display_links = ('','fist_name','last_name','phone_number','email','adress')
-    search_fields = ('fist_name','last_name')
-    list_filter = ('fist_name','last_name')
-class AlunoAdimin(admin.ModelAdmin):
-    list_display = ('fist_name_aluno', 'last_name_aluno', 'phone_number_aluno', 'email_aluno', 'cpf_aluno', 'birthday_aluno')
-    list_display_links = ('fist_name_aluno', 'last_name_aluno', 'phone_number_aluno', 'email_aluno')
-    search_fields = ('fist_name', 'last_name')
-    list_filter = ('fist_name', 'last_name')
-class ProfessorAdimin(admin.ModelAdmin):
-    list_display = ('id', 'fist_name', 'last_name', 'phone_number', 'email', 'adress', 'cpf', 'birthday')
-    list_display_links = ('', 'fist_name', 'last_name', 'phone_number', 'email', 'adress')
-    search_fields = ('fist_name', 'last_name')
-    list_filter = ('fist_name', 'last_name')
+from .models import Responsavel, Aluno, Professor, Turma, Contrato
 
-admin.site.register(Responsavel)
-admin.site.register(Aluno)
-admin.site.register(Professor)
+class ResponsaveisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone_number', 'email', 'address', 'cpf', 'birthday')
+    list_display_links = ('id', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('first_name_aluno', 'last_name_aluno', 'phone_number_aluno', 'email_aluno', 'cpf_aluno', 'birthday_aluno', 'class_choices')
+    list_display_links = ('first_name_aluno', 'last_name_aluno')
+    search_fields = ('first_name_aluno', 'last_name_aluno')
+    list_filter = ('class_choices',)
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone_number', 'email', 'address', 'cpf', 'registration_number')
+    list_display_links = ('id', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
+
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('turma', 'itinerario', 'representante', 'vice_representante')
+    list_display_links = ('turma',)
+    search_fields = ('turma', 'itinerario')
+    list_filter = ('itinerario',)
+
+class ContratoAdmin(admin.ModelAdmin):
+    list_display = ('turma', 'aluno', 'responsavel', 'email_responsavel')
+    list_display_links = ('turma', 'aluno')
+    search_fields = ('aluno__first_name_aluno', 'responsavel__first_name')
+    list_filter = ('turma',)
+
+# Registrando os modelos no admin
+admin.site.register(Responsavel, ResponsaveisAdmin)
+admin.site.register(Aluno, AlunoAdmin)
+admin.site.register(Professor, ProfessorAdmin)
+admin.site.register(Turma, TurmaAdmin)
+admin.site.register(Contrato, ContratoAdmin)
--- aula_12-03-2025/P-S-E_pycharm/SecretariaEscolar/projeto/models.py
+++ aula_12-03-2025/P-S-E_pycharm/SecretariaEscolar/projeto/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+
 class Responsavel(models.Model):
-    fist_name = models.CharField(max_length=50, verbose_name="nome do responsavel")
-    last_name = models.CharField(max_length=50, verbose_name="sobre nome do responsavel")
-    phone_number = models.CharField(max_length=15, verbose_name="Nº do celular (--) ---- ---- do responsavel")
-    email = models.CharField(max_length=100, verbose_name="email do responsavel")
-    adress = models.CharField(max_length=100, verbose_name="endereço do responsavel")#coloque um validador/buscador de senha
-    cpf = models.CharField(max_length=11, unique= True, verbose_name="cpf do responsavel")
+    first_name = models.CharField(max_length=50, verbose_name="Nome do responsável")
+    last_name = models.CharField(max_length=50, verbose_name="Sobrenome do responsável")
+    phone_number = models.CharField(max_length=15, verbose_name="Nº do celular (--) ---- ---- do responsável")
+    email = models.CharField(max_length=100, verbose_name="Email do responsável")
+    address = models.CharField(max_length=100, verbose_name="Endereço do responsável")
+    cpf = models.CharField(max_length=11, unique=True, verbose_name="CPF do responsável")
     birthday = models.DateField()
 
     def __str__(self):
-        return self.fist_name
+        return self.first_name
 
 class Aluno(models.Model):
-    TURMA_CHOICES=(
+    TURMA_CHOICES = (
         ("1A", "1º Ano A"),
         ("1B", "1º Ano B"),
         ("1C", "1º Ano C"),
@@ -22,25 +23,60 @@ class Aluno(models.Model):
         ("3A", "3º Ano A"),
         ("3B", "3º Ano B"),
         ("3C", "3º Ano C"),
-
     )
-    fist_name_aluno = models.CharField(max_length=50, verbose_name="Nome")
+    first_name_aluno = models.CharField(max_length=50, verbose_name="Nome")
     last_name_aluno = models.CharField(max_length=50, verbose_name="Sobrenome")
     phone_number_aluno = models.CharField(max_length=15, verbose_name="Nº do celular (--) ---- ---- do aluno")
-    email_aluno = models.CharField(max_length=100, verbose_name="email do aluno")
-    cpf_aluno = models.CharField(max_length=11, unique=True, verbose_name="cpf do aluno")
+    email_aluno = models.CharField(max_length=100, verbose_name="Email do aluno")
+    cpf_aluno = models.CharField(max_length=11, unique=True, verbose_name="CPF do aluno")
     birthday_aluno = models.DateField()
-    class_choices= models.CharField(max_length=2,choices=TURMA_CHOICES,blank=True,null=False)
+    class_choices = models.CharField(max_length=2, choices=TURMA_CHOICES, blank=True, null=False)
+
     def __str__(self):
-        return self.fist_name_aluno
+        return self.first_name_aluno
 
 class Professor(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name="Nome")
     last_name = models.CharField(max_length=50, verbose_name="Sobrenome")
     phone_number = models.CharField(max_length=15, verbose_name="Nº do celular (--) ---- ----")
     email = models.EmailField(max_length=100, verbose_name="Email do professor")
     address = models.CharField(max_length=100, verbose_name="Endereço")
-    cpf = models.CharField(max_length=11, unique= True, verbose_name="cpf do professor")
+    cpf = models.CharField(max_length=11, unique=True, verbose_name="CPF do professor")
     registration_number = models.CharField(max_length=20, unique=True, verbose_name="Número de Registro")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} (Registro: {self.registration_number})"
+
+class Turma(models.Model):
+    TURMA_CHOICES = (
+        ("1A", "1º Ano A"),
+        ("1B", "1º Ano B"),
+        ("1C", "1º Ano C"),
+        ("2A", "2º Ano A"),
+        ("2B", "2º Ano B"),
+        ("2C", "2º Ano C"),
+        ("3A", "3º Ano A"),
+        ("3B", "3º Ano B"),
+        ("3C", "3º Ano C"),
+    )
+    ITINERARIO_CHOICES = (
+        ("DS", "Desenvolvimento de Sistemas"),
+        ("CN", "Ciências da Natureza"),
+        ("J", "Jogos"),
+    )
+    turma = models.CharField(max_length=2, choices=TURMA_CHOICES, verbose_name="Turma")
+    itinerario = models.CharField(max_length=2, choices=ITINERARIO_CHOICES, verbose_name="Itinerário")
+    representante = models.ForeignKey(Aluno, on_delete=models.SET_NULL, related_name="representante_turma", null=True, blank=True, verbose_name="Representante")
+    vice_representante = models.ForeignKey(Aluno, on_delete=models.SET_NULL, related_name="vice_representante_turma", null=True, blank=True, verbose_name="Vice-Representante")
+
+    def __str__(self):
+        return f"{self.turma} - {self.get_itinerario_display()}"
+
+class Contrato(models.Model):
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE, verbose_name="Turma/Itinerário")
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE, verbose_name="Nome do Aluno")
+    responsavel = models.ForeignKey(Responsavel, on_delete=models.CASCADE, verbose_name="Nome do Responsável")
+    email_responsavel = models.EmailField(verbose_name="Email do Responsável")
+
+    def __str__(self):
+        return f"Contrato: {self.aluno} - {self.turma}"
